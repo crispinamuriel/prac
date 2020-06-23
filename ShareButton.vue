61bf773f0fe2c6bfd74fb82d77daecc6ae848021
crispinamuriel <crispinamuriel@gmail.com>
2020-06-23 16:56:06 -0400

    working on function

--- ShareButton.vue
+++ ShareButton.vue
@@ -1,24 +1,47 @@
 <template>
-  <ShareButtonTemplate></ShareButtonTemplate>
+  <ShareButtonTemplate :url="url" :text="text">
+    <span v-if="!fillColor" slot="shareIcon" class="has-text-white has-background-facebook button full-width is-hidden-print">
+      <span class="icon mr-xs facebook" /><span>{{ buttonText }} </span>
+    </span>
+  </ShareButtonTemplate>
 </template>
+
 <script>
-import ShareButtonTemplate from "./ShareButtonTemplate";
+import ShareButtonTemplate from './ShareButtonTemplate'
 export default {
   components: {
     ShareButtonTemplate
   },
-  data() {
-    return {
-      url: "www.google.com"
+  props: {
+    /**
+     * link to share
+     */
+    url: {
+      type: String,
+      default: null
+    },
+    /**
+     * text for shared item
+     */
+    text: {
+      type: String,
+      default: ''
+    },
+    /**
+     * text to include on the share button
+     */
+    buttonText: {
+      type: String,
+      default: 'Share'
     }
   }
 }
 </script>
-  <style>
+<style lang="scss" scoped>
   .fb-share {
     width: 500px;
     height: 200px;
     background-color: red;
     color: yellow;
   }
-  </style>
+</style>
